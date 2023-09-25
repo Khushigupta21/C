fbd849aa3f66127ac0f4bae36cabd02cbfe50182
Khushigupta21 <120445117+Khushigupta21@users.noreply.github.com>
2023-09-25 16:05:12 +0530

    inline_function.cpp
    Program to find the multiplication values and the cubic values using
    inline function

--- C++ Assignment/inline_function.cpp
+++ C++ Assignment/inline_function.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
+class line{
+    public:
+    inline float multi(float x, float y){
+        return (x*y);
+    }
+    inline float cube(float x){
+        return (x*x*x);
+    }
+};
 int main(){
-
+    line obj;
+    cout<<obj.multi(2,3)<<endl;
+    cout<<obj.cube(5)<<endl;
     return 0;
 }
